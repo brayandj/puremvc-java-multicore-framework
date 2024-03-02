230b2decabbface05a67553603c19cfcb38a0475
Brayan Díaz Jiménez <114362572+brayandj@users.noreply.github.com>
2024-03-02 17:23:57 -0600

    Actualizar Notification.java

--- src/main/java/org/puremvc/java/multicore/patterns/observer/Notification.java
+++ src/main/java/org/puremvc/java/multicore/patterns/observer/Notification.java
@@ -10,39 +10,39 @@ package org.puremvc.java.multicore.patterns.observer;
 import org.puremvc.java.multicore.interfaces.INotification;
 
 /**
- * <P>Una implementación base de <code>INotification</code>.</P>
+ * Una implementación base de INotification.
  *
- * <P>PureMVC no depende de modelos de eventos subyacentes como
+ * PureMVC no depende de modelos de eventos subyacentes como
  * el proporcionado con Flash y ActionScript 3 no tiene un modelo
- * de eventos inherente.</P>
+ * de eventos inherente.
+ *  
+ * El patrón Observer como se implementa dentro de PureMVC existe
+ * para soportar la comunicación orientada a eventos entre la  
+ * aplicación y los actores de la tríada MVC.
  *
- * <P>El patrón Observer como se implementa dentro de PureMVC existe
- * para soportar la comunicación orientada a eventos entre la
- * aplicación y los actores de la tríada MVC.</P>
- *
- * <P>Las Notificaciones no pretenden reemplazar a los Eventos
- * en Flex/Flash/Apollo. Generalmente, los implementadores de <code>IMediator</code>
+ * Las Notificaciones no pretenden reemplazar a los Eventos
+ * en Flex/Flash/Apollo. Generalmente, los implementadores de IMediator  
  * colocan detectores de eventos en sus componentes de vista, que
- * luego controlan de la forma habitual. Esto puede llevar a la transmisión de <code>Notification</code>s
- * para activar <code>ICommand</code>s o para comunicarse con otros <code>IMediators</code>. Las instancias <code>IProxy</code> y <code>ICommand</code>
- * se comunican entre sí y con <code>IMediator</code>s
- * transmitiendo <code>INotification</code>s.</P>
+ * luego controlan de la forma habitual. Esto puede llevar a la transmisión de Notifications
+ * para activar ICommands o para comunicarse con otros IMediators. Las instancias IProxy y ICommand
+ * se comunican entre sí y con IMediators
+ * transmitiendo INotifications.
  *
- * <P>Una diferencia clave entre los <code>Event</code>s de Flash y las
- * <code>Notification</code>s de PureMVC es que los <code>Event</code>s siguen
+ * Una diferencia clave entre los Events de Flash y las
+ * Notifications de PureMVC es que los Events siguen
  * el patrón 'Chain of Responsibility', 'burbujeando' por la jerarquía de visualización
- * hasta que algún componente principal maneja el <code>Event</code>, mientras que
- * las <code>Notification</code>s de PureMVC siguen un patrón 'Publish/Subscribe'.
- * Las clases PureMVC no necesitan estar relacionadas entre sí en una
+ * hasta que algún componente principal maneja el Event, mientras que
+ * las Notifications de PureMVC siguen un patrón 'Publish/Subscribe'.
+ * Las clases PureMVC no necesitan estar relacionadas entre sí en una  
  * relación padre / hijo para comunicarse entre sí
- * usando <code>Notification</code>s.</P>
- *
+ * usando Notifications.
+ * 
  * @see Observer Observer
  *
  */
 public class Notification implements INotification {
 
-    // el nombre de la instancia de notificación
+    // el nombre de la instancia de notificación 
     private String name;
 
     // el tipo de la instancia de notificación
@@ -52,11 +52,11 @@ public class Notification implements INotification {
     private Object body;
 
     /**
-     * <P>Constructor.</P>
-     *
-     * @param name nombre de la instancia <code>Notification</code>. (requerido)
-     * @param body el cuerpo de la <code>Notification</code>.
-     * @param type el tipo de la <code>Notification</code>
+     * Constructor.
+     
+     * @param name nombre de la instancia Notification. (requerido)
+     * @param body el cuerpo de la Notification.  
+     * @param type el tipo de la Notification
      */
     public Notification(String name, Object body, String type) {
         this.name = name;
@@ -65,43 +65,43 @@ public class Notification implements INotification {
     }
 
     /**
-     * <P>Constructor.</P>
-     *
-     * @param name nombre de la instancia <code>Notification</code>. (requerido)
-     * @param body el cuerpo de la <code>Notification</code>.
+     * Constructor.
+     
+     * @param name nombre de la instancia Notification. (requerido) 
+     * @param body el cuerpo de la Notification.
      */
     public Notification(String name, Object body) {
         this(name, body, null);
     }
 
     /**
-     * <P>Constructor.</P>
-     *
-     * @param name nombre de la instancia <code>Notification</code>. (requerido)
+     * Constructor.
+     
+     * @param name nombre de la instancia Notification. (requerido)
      */
     public Notification(String name) {
         this(name, null, null);
     }
 
     /**
-     * <P>Obtener el nombre de la instancia <code>Notification</code>.</P>
-     *
-     * @return el nombre de la instancia <code>Notification</code>.
+     * Obtener el nombre de la instancia Notification.
+     
+     * @return el nombre de la instancia Notification.
      */
     public String getName() {
         return name;
     }
 
     /**
-     * <P>Establecer el cuerpo de la instancia <code>Notification</code>.</P>
+     * Establecer el cuerpo de la instancia Notification.
      */
     public void setBody(Object body) {
         this.body = body;
     }
 
     /**
-     * <P>Obtener el cuerpo de la instancia <code>Notification</code>.</P>
-     *
+     * Obtener el cuerpo de la instancia Notification.
+     
      * @return el objeto body.
      */
     public Object getBody() {
@@ -109,15 +109,15 @@ public class Notification implements INotification {
     }
 
     /**
-     * <P>Establecer el tipo de la instancia <code>Notification</code>.</P>
+     * Establecer el tipo de la instancia Notification.
      */
     public void setType(String type) {
         this.type = type;
     }
 
     /**
-     * <P>Obtener el tipo de la instancia <code>Notification</code>.</P>
-     *
+     * Obtener el tipo de la instancia Notification.
+     
      * @return el tipo
      */
     public String getType() {
@@ -125,9 +125,9 @@ public class Notification implements INotification {
     }
 
     /**
-     * <P>Obtener la representación en cadena de la instancia <code>Notification</code>.</P>
-     *
-     * @return la representación en cadena de la instancia <code>Notification</code>.
+     * Obtener la representación en cadena de la instancia Notification.
+     
+     * @return la representación en cadena de la instancia Notification.
      */
     public String toString() {
         return new StringBuilder("Notification Name: " + getName())
